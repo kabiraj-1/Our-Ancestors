1095e2c091e7b51a3f7a0e50fcc1e358d1b90eff
kabi Raj bhatt <bhattkabiraj255@gmail.com>
2025-03-04 22:30:16 +0545

    Update Message.js
    Signed-off-by: kabi Raj bhatt   <bhattkabiraj255@gmail.com>

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now },
-    read: { type: Boolean, default: false }
-});
+const MessageSchema = new mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    message: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Message', messageSchema);
+module.exports = mongoose.model("Message", MessageSchema);
