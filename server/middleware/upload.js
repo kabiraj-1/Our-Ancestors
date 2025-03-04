c788a579c8e237526f60c591aca195f369f2ceea
kabi Raj bhatt <bhattkabiraj255@gmail.com>
2025-03-04 22:47:20 +0545

    Update upload.js
    Signed-off-by: kabi Raj bhatt   <bhattkabiraj255@gmail.com>

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,18 +1,14 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../../client/assets/uploads'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
+  destination: (req, file, cb) => {
+    cb(null, "client/assets/uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-const upload = multer({
-    storage,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5MB
-});
+const upload = multer({ storage });
 
 module.exports = upload;
