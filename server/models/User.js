3da2a1d784dc472e72702f58add77c6a11256bb5
kabi Raj bhatt <bhattkabiraj255@gmail.com>
2025-05-08 17:52:03 +0545

    Update User.js
    Signed-off-by: kabi Raj bhatt   <bhattkabiraj255@gmail.com>

--- server/models/User.js
+++ server/models/User.js
@@ -5,13 +5,10 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isVerified: { type: Boolean, default: false },
-  avatar: String,
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   createdAt: { type: Date, default: Date.now }
 });
 
-// Password hashing middleware
 userSchema.pre('save', async function(next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 10);
