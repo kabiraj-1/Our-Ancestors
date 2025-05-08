2d28405f573475bcde7d3451d248ef28fb954502
kabi Raj bhatt <bhattkabiraj255@gmail.com>
2025-05-08 17:54:23 +0545

    Update authController.js
    Signed-off-by: kabi Raj bhatt   <bhattkabiraj255@gmail.com>

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,36 +1,31 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        
-        // Check if user exists
-        if (await User.findOne({ $or: [{ username }, { email }] })) {
-            return res.status(400).json({ error: 'User already exists' });
-        }
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({ username, email, password });
+    await user.save();
+    
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-        // Create new user
-        const user = new User({
-            username,
-            email,
-            password,
-            avatar: req.file ? `/assets/uploads/${req.file.filename}` : null
-        });
-
-        await user.save();
-
-        // Generate JWT
-        const token = jwt.sign(
-            { userId: user._id }, 
-            process.env.JWT_SECRET, 
-            { expiresIn: '7d' }
-        );
-
-        res.status(201).json({ token });
-    } catch (err) {
-        console.error('Registration error:', err);
-        res.status(500).json({ error: 'Server error' });
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    
+    if (!user || !(await user.matchPassword(password))) {
+      throw new Error('Invalid credentials');
     }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.json({ token });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
 };
